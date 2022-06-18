3f4d38a65fdb0cc04727df18498beef533ca019e
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-06-18 16:14:04 -0300

    Vo telephone

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
     private String name;
     private Cpf cpf;
     private Email email;
+    private List<Telephone> telephones = new ArrayList<>();
+
+    public void addTelephones(String ddd, String number) {
+        telephones.add(new Telephone(ddd, number));
+    }
 }
---
+++ src/main/java/Telephone.java
@@ -0,0 +1,22 @@
+public class Telephone {
+    private String ddd;
+    private String number;
+
+    public Telephone(String ddd, String number) {
+        if (ddd == null || number == null) {
+            throw new IllegalArgumentException("DDD and Number is required!");
+        }
+
+        if (!ddd.matches("\\d{2}")) {
+            throw new IllegalArgumentException("DDD invalid!");
+        }
+
+        if (!number.matches("\\d{8}|\\d{9}")) {
+            throw new IllegalArgumentException("Number invalid!");
+        }
+        this.ddd = ddd;
+        this.number = number;
+    }
+
+
+}
