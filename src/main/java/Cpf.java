d60eda4a86986e632e863d9e43a9006dd1c3c363
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-06-18 15:43:47 -0300

    Entity student and vo email and cpf

---
+++ src/main/java/Cpf.java
@@ -0,0 +1,10 @@
+public class Cpf {
+    private String number;
+
+    public Cpf(String number) {
+        if (number == null || !number.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")) {
+            throw new IllegalArgumentException("CPF invalid!");
+        }
+        this.number = number;
+    }
+}
---
+++ src/main/java/Email.java
@@ -0,0 +1,10 @@
+public class Email {
+    private String address;
+
+    public Email(String address) {
+        if (address == null || !address.matches("^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
+            throw new IllegalArgumentException("E-mail invalid!");
+        }
+        this.address = address;
+    }
+}
