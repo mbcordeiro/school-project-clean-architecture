b570b05c9060029a437817c082a3946368091808
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-06-20 23:23:54 -0300

    Student factory

--- src/main/java/Indication.java
+++ src/main/java/Indication.java
@@ -1,9 +1,25 @@
+import java.time.LocalDateTime;
+
 public class Indication {
     private Student indicator;
     private Student indicated;
+    private LocalDateTime indicationDate;
 
     public Indication(Student indicator, Student indicated) {
         this.indicator = indicator;
         this.indicated = indicated;
+        this.indicationDate = LocalDateTime.now();
+    }
+
+    public Student getIndicator() {
+        return indicator;
+    }
+
+    public Student getIndicated() {
+        return indicated;
+    }
+
+    public LocalDateTime getIndicationDate() {
+        return indicationDate;
     }
 }
