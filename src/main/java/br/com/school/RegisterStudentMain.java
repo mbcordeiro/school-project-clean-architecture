b888720dc289fc5f745acbccfa3e24582ea2e199
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-07-11 00:12:35 -0300

    Add listiner

--- src/main/java/br/com/school/RegisterStudentMain.java
+++ src/main/java/br/com/school/RegisterStudentMain.java
@@ -1,8 +1,11 @@
-package br.com.school.academic;
+package br.com.school;
 
 import br.com.school.academic.application.student.register.RegisterStudent;
 import br.com.school.academic.application.student.register.RegisterStudentDto;
 import br.com.school.academic.domain.student.LogListener;
+import br.com.school.gamification.application.GenerateRookieStudentBadge;
+import br.com.school.gamification.infra.badge.BadgeRepositoryInMemory;
+import br.com.school.shared.domain.event.Event;
 import br.com.school.shared.domain.event.PublisherEvent;
 import br.com.school.academic.infra.student.StudentRepositoryInMemory;
 
@@ -14,6 +17,7 @@ public class RegisterStudentMain {
 
         final var publisherEvent = new PublisherEvent();
         publisherEvent.addListener(new LogListener());
+        publisherEvent.addListener(new GenerateRookieStudentBadge(new BadgeRepositoryInMemory()));
         final var registerStudent = new RegisterStudent(new StudentRepositoryInMemory(), publisherEvent);
         registerStudent.execute(new RegisterStudentDto(name, cpf, email));
     }
